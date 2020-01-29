d325943973b647b156e9e7f9484f1dd705754339
Andrew Rissing <arissing@alkamitech.com>
2020-01-29 09:30:42 -0600

    Updated nuget packages. Updated project to .NET 4.8 and the SDK project style.

--- src/ConsoleCommandLine.cs
+++ src/ConsoleCommandLine.cs
@@ -25,7 +25,6 @@
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
 #endregion
-using System;
 using System.Collections.Generic;
 using Microsoft.Extensions.CommandLineUtils;
 
--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -31,10 +31,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FeedAudit")]
+[assembly: AssemblyTitle("SynchroFeed.Console")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyProduct("FeedAudit")]
+[assembly: AssemblyProduct("SynchroFeed.Console")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
